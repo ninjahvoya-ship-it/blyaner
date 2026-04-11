f172472491e3aa91da092c0ab3218b4e9ea6ae09
ninjahvoya-ship-it <ninjahvoya-ship-it@users.noreply.github.com>
2026-04-11 01:13:17 +0000

    UI: change week page outer container background to white to fix transparent gaps

--- app/week/page.tsx
+++ app/week/page.tsx
@@ -5,7 +5,7 @@ import WeekGrid from "../../components/WeekGrid";
 export default function WeekPage() {
   return (
     <div className="min-h-screen p-4 md:p-6 bg-[#E8E6F0]">
-      <div className="max-w-[1440px] mx-auto bg-main-bg rounded-[32px] shadow-xl overflow-hidden flex min-h-[93vh]">
+      <div className="max-w-[1440px] mx-auto bg-white rounded-[32px] shadow-xl overflow-hidden flex min-h-[93vh]">
         <Sidebar />
         <div className="flex-1 flex flex-col">
           <Header title="Твоя неделя" />
